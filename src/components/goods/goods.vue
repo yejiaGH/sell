447540a91e5ce92695d64b51995cf9cbe6bd4631
Ye Jia <ye.jia.alice@gmail.com>
2017-02-12 21:00:01 +0800

    增加vue-router

---
+++ src/components/goods/goods.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="goods">
+		我是goods
+	</div>
+</template>
+
+<script type="text/ecmascript-6">
+	export default {};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" >
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App';
+import goods from 'components/goods/goods';
+import ratings from 'components/ratings/ratings';
+import seller from 'components/seller/seller';
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App },
+import 'common/stylus/index.styl';
+
+Vue.use(VueRouter);
+
+const app = Vue.extend(App);
+const router = new VueRouter({
+  linkActiveClass: 'active',
 });
+router.map({
+  '/goods': {
+    component: goods,
+  },
+  '/ratings': {
+    component: ratings,
+  },
+  '/seller': {
+    component: seller,
+  },
+});
+router.start(app, '#app');
+router.go('/goods');
